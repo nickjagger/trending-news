6ba2dde57fb723771a8484ec6935779da108723b
Nick Jagger <nicholas.jagger@trivadis.com>
2015-12-21 13:23:10 +0100

    Removed redundant respository

--- trending-news-service/src/main/java/com/nmj/trendingNews/respositories/TwitterRepository.java
+++
@@ -1,7 +0,0 @@
-package com.nmj.trendingNews.respositories;
-
-public interface TwitterRepository {//extends PagingAndSortingRepository<Tweet, Long> {
-
-	//    @Query("select fortune from Fortune fortune order by RAND()")
-	//    public List<Tweet> randomFortunes(Pageable pageable);
-}
